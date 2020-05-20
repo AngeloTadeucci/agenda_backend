a180b05aacc172ff4717ea0740911b5d4cddc18d
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2020-05-19 21:49:26 -0300

    insert

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 
 const app = express();
 
-app.get("/", async (request, response) => {
+app.get("/", async (req, res) => {
   const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -15,14 +15,36 @@ app.get("/", async (request, response) => {
     const result = await client.query("SELECT * FROM tipocontato");
     const results = result.rows;
     client.end();
-    return response.json({ results });
+    return res.json({ results });
   } catch (err) {
     console.error(err);
-    return response.json(err);
+    return res.json(err);
   }
 });
 
-app.post("/", (request, response) => {});
+app.post("/", (req, res) => {
+  const { descricao } = req.body;
+
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  try {
+    const text = "INSERT INTO public.tipocontato (descricao) VALUES($1);";
+    const params = [descricao];
+
+    const client = await pool.connect();
+    const result = await client.query(text, params);
+    const results = result.rows;
+    client.end();
+    return res.json({ results });
+  } catch (err) {
+    console.error(err);
+    return res.json(err);
+  }
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("Servidor iniciado");

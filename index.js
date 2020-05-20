cb7119f1a4c0c288820c5a23d8a26458aa06ea7c
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2020-05-19 21:19:23 -0300

    fix: faltando async

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express  = require("express");
+const express = require("express");
 const { Pool } = require("pg");
 
 const app = express();
 
-
-
-app.get("/", (request, response) => {
+app.get("/", async (request, response) => {
   const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -14,13 +12,13 @@ app.get("/", (request, response) => {
   });
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM contato');
+    const result = await client.query("SELECT * FROM contato");
     const results = result.rows;
     client.end();
-    return response.json(results)
+    return response.json({ results });
   } catch (err) {
     console.error(err);
-    return response.json(err)
+    return response.json(err);
   }
 });
 

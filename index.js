9c7bd80a3b248b384d04401ba2735ef97a164e9e
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2020-05-19 22:01:28 -0300

    delete

--- index.js
+++ index.js
@@ -34,10 +34,34 @@ app.post("/", async (req, res) => {
   });
   try {
     const text = "INSERT INTO public.tipocontato (descricao) VALUES($1);";
-    const params = [descricao];
+    const values = [descricao];
 
     const client = await pool.connect();
-    const result = await client.query(text, params);
+    const result = await client.query(text, values);
+    const results = result.rows;
+    client.end();
+    return res.json({ results });
+  } catch (err) {
+    console.error(err);
+    return res.json(err);
+  }
+});
+
+app.delete("/:id", async (req, res) => {
+  const { id } = request.params;
+
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  try {
+    const text = "DELETE FROM public.tipocontato WHERE id=$1;";
+    const values = [id];
+
+    const client = await pool.connect();
+    const result = await client.query(text, values);
     const results = result.rows;
     client.end();
     return res.json({ results });

2bbf6bcf5c1f83fe7624b9fa026c56e1e471d6f4
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2020-05-26 20:52:10 -0300

    typo

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const eventoRounter = require("./evento.routes");
 const routes = Router();
 
 routes.use("/tipocontato", tipoContatoRouter);
-routes.use("/evento", eventoRounter);
+routes.use("/evento", eventoRouter);
 // routes.use("/contato", tipoContatoRouter);
 
 module.exports = routes;
